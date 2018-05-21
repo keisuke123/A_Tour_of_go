d1b6d76fa305d81f7522bc385ff91114c370133b
Kamiya Keisuke <ozilikepop@gmail.com>
2018-05-21 19:36:52 +0900

    add range.go

---
+++ Basics/More_types/range.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	// each_with_indexのようなもの
+	for index, value := range pow {
+		fmt.Printf("2**%d = %d\n", index, value)
+	}
+}
